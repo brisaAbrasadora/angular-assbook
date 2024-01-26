dbb94cd1b239625fd9282ee23d01ce7f83d2c19a
Daniel Rabago Prades <danielrabagoprades@gmail.com>
2024-01-26 17:24:04 +0100

    Minor update on Login to make it reactive and have validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,26 +1,32 @@
 import { Component, inject } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormControl, NonNullableFormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
 import { UserLogin } from "../interfaces/user";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: "login",
     standalone: true,
-    imports: [FormsModule],
+    imports: [CommonModule, ReactiveFormsModule],
     templateUrl: "./login.component.html",
     styleUrl: "./login.component.css",
 })
 export class LoginComponent {
+    #formBuilder = inject(NonNullableFormBuilder);
     #router = inject(Router);
     #authService = inject(AuthService);
-    email = "";
-    password = "";
+    email: FormControl<string> = this.#formBuilder.control("", [Validators.required, Validators.email]);
+    password: FormControl<string> = this.#formBuilder.control("", [Validators.required]);
+
+    loginForm = this.#formBuilder.group({
+        email: this.email,
+        password: this.password
+    });
 
     login() {
         const data: UserLogin = {
-            email: this.email,
-            password: this.password,
+            ...this.loginForm.getRawValue()
         };
         this.#authService.login(data).subscribe({
             next: () => {
@@ -30,5 +36,12 @@ export class LoginComponent {
                 console.error(err.error.message);
             }
         });
+    }
+
+    validClasses(control: FormControl, validClass: string, errorClass: string): { [key: string]: boolean } {
+        return {
+            [validClass]: control.touched && control.valid && control.value,
+            [errorClass]: control.touched && control.invalid
+        };
     }
 }
